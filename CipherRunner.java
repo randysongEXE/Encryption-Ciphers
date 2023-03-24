0ebe01f4fbb1b139b6742392c909d74b36e757fc
Randy Song <127687854+randysongEXE@users.noreply.github.com>
2023-03-23 22:57:30 -0500

    Update CipherRunner.java

--- CipherRunner.java
+++ CipherRunner.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class CipherRunner {
 
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+        Scanner key = new Scanner(System.in);
 
         VigenereCipher vigenereCipher = new VigenereCipher();
         CaesarCipher caesarCipher = new CaesarCipher();
@@ -23,8 +23,8 @@ public class CipherRunner {
 
 
 
-            int choice = scanner.nextInt();
-            scanner.nextLine();
+            int choice = key.nextInt();
+            key.nextLine();
 
             if (choice == 0) {
                 System.out.println("Exiting...");
@@ -32,26 +32,26 @@ public class CipherRunner {
             }
 
             System.out.print("Please enter the message to encrypt: ");
-            String message = scanner.nextLine();
+            String message = key.nextLine();
 
             switch (choice) {
                 case 1:
                     System.out.print("Please enter the key for Vigenere Cipher: ");
-                    String key = scanner.nextLine();
-                    System.out.println("Encrypted message: " + vigenereCipher.encrypt(message, key));
-                    System.out.println("Decrypted message: " + vigenereCipher.decrypt(vigenereCipher.encrypt(message, key), key));
+                    String key1 = key.nextLine();
+                    System.out.println("Encrypted message: " + vigenereCipher.encrypt(message, key1));
+                    System.out.println("Decrypted message: " + vigenereCipher.decrypt(vigenereCipher.encrypt(message, key1), key1));
                     break;
                 case 2:
                     System.out.print("Please enter the shift for Caesar Cipher: ");
-                    int shift = scanner.nextInt();
-                    scanner.nextLine();
+                    int shift = key.nextInt();
+                    key.nextLine();
                     System.out.println("Encrypted message: " + caesarCipher.encrypt(message, shift));
                     System.out.println("Decrypted message: " + caesarCipher.decrypt(caesarCipher.encrypt(message, shift), shift));
                     break;
                 case 3:
                     System.out.print("Please enter the number of rails for Rail Fence Cipher: ");
-                    int rails = scanner.nextInt();
-                    scanner.nextLine();
+                    int rails = key.nextInt();
+                    key.nextLine();
                     System.out.println("Encrypted message: " + railFenceCipher.encrypt(message, rails));
                     System.out.println("Decrypted message: " + railFenceCipher.decrypt(railFenceCipher.encrypt(message, rails), rails));
                     break;
